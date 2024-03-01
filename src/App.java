2b605ccadd7202988ec98230ab1aebcb8d772ba0
zhishixuegou <x00217230231@njit.edu.cn>
2024-03-01 17:14:30 +0800

    wrt

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, W2Y32!");
+        System.out.println("Hello, 9999999!");
     }
 }
